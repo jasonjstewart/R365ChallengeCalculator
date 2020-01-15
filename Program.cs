e2f1530908204d6cf9e373a31902137a421d09fa
Jason Stewart <jason.iowa@gmail.com>
2020-01-14 17:35:15 -0700

    Commit requirement 2

--- Program.cs
+++ Program.cs
@@ -12,13 +12,14 @@ namespace Challenge_Calculator
             String[] numbers;
             int sum=0;
             int i = 0;
-            Console.WriteLine("Please enter two numbers separated by commas.");
+            Console.WriteLine("Please enter numbers separated by commas.");
             input = Console.ReadLine();
             numbers=input.Split(',');
-            if (numbers.Length>2)
-            {
-                throw new System.ArgumentException("Too many numbers specified", "original");
-            }
+            //Took this out for Requirement 2
+            //if (numbers.Length>2)
+            //{
+            //    throw new System.ArgumentException("Too many numbers specified", "original");
+            //}
             foreach (var num in numbers){                
                 sum = Convert.ToInt32(int.TryParse(num, out i) ? num : "0") + sum;
             }
